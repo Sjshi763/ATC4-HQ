80a1d031e085444c35a07c06f219c0735a32f78e
Sjshi763 <qq3258819795@163.com>
2025-01-20 00:33:44 +0800

    8

---
+++ for others airport/others-airport-start.py
@@ -0,0 +1,9 @@
+#上一次是第一次用c/c++，这一次第一次用py
+#依然是写一下要求
+#1、找到在子文件夹下的RJAA.dll复制到暂存
+#2、改名为XPACK.dll
+#3、复制到游戏更目录
+#4、删了XPACK.dll
+#5、用命令行用locale Emulaturv启动ATC4
+#ATC4和程序在同一文件夹为AXA.exe
+#Locale Emulatorv 在 C:\LE\LEProc.exe
