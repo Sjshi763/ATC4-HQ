15645d44d12da0e604ca0e71f8a2ad0934a80ca8
Sjshi763 <qq3258819795@163.com>
2025-01-30 18:02:17 +0800

    9

--- game start/game-start.py
+++ game start/game-start.py
@@ -1,34 +1,33 @@
 #这个程序的目标是启动！游戏时让玩家选择启动方式，然后加载游戏。
 import tkinter as tk
 import subprocess
-
+#变量们
+C=10
+D=1
+F=200
+E= F-(C*10+0)
 # 创建主窗口
 root = tk.Tk()
 root.title("游戏启动器的启动器")
-
 # 设置窗口大小
 root.geometry("400x300")
-
+root.resizable(False, False)
 # 创建一个函数
 def B():
     subprocess.Popen(["others-airport-start.exe"]) 
-
+    root.quit()
 # 创建一个函数
 def A():
     subprocess.Popen(["RJOO-start.exe"]) 
-
+    root.quit()
 # 创建一个标签
-label = tk.Label(root, text="选择你的机场！", font=("Arial", 24))
+label = tk.Label(root, text="选择你的机场！", font=("华康翩翩体W5-A", 24),width=20, height=2)
 label.pack(pady=20)
-
 # 创建一个按钮
-button = tk.Button(root, text="其他的", font=("Arial", 14),command=B)
-button.pack(pady=10, side=tk.LEFT)
-
+button = tk.Button(root, text="其他的", font=("华康翩翩体W5-A", 14),command=B,width=C, height=D)
+button.place(x=F, y=F)
 #创建另一个按钮
-button = tk.Button(root, text="RJOO", font=("Arial", 14),command=A)
-button.pack(pady=10, side=tk.RIGHT)
-
-
+button = tk.Button(root, text="RJOO", font=("华康翩翩体W5-A", 14),command=A,width=C, height=D)
+button.place(x=E, y=F)
 # 运行主循环
 root.mainloop()
