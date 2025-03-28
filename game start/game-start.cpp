5f99e45884f5b9a532d1197726791b709a5bdd27
Sjshi763 <qq3258819795@163.com>
2025-03-29 01:36:38 +0800

    2

--- game start/game-start.cpp
+++ game start/game-start.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <windows.h>
 namespace master {
-	int a = GetSystemMetrics(SM_CXSCREEN)/4; //这个
+	int a = GetSystemMetrics(SM_CXSCREEN)/4; //这个变量int a是用户显示屏高的四分之一
+	int b = 
 }
 int main() {
 	using namespace master;
-	initgraph(master::a, master::b);
-	
+	loadimage(NULL , "/ATC4.ico"); //加载图片
+	initgraph(,);
+	putimage(a, 0, "/ATC4.ico"); //在屏幕上显示图片
 	closegraph();
 	return 0;
 }
