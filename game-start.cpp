d186e2994844cfba8b87543bca5d16b7d788f940
Sjshi763 <qq3258819795@163.com>
2025-03-29 08:39:48 +0800

    7

--- game-start.cpp
+++ game-start.cpp
@@ -4,31 +4,15 @@
 #include <windows.h>
 namespace master {
 	int a = GetSystemMetrics(SM_CXSCREEN); //这个变量int a是用户显示屏高
-	int b = GetSystemMetrics(SM_CYSCREEN); //这个变量int b是用户显示屏宽
+	int b = a /4;
 }
 int main() {
 	using namespace master;
+	SetConsoleOutputCP(CP_UTF8); //设置控制台输出编码为UTF-8
+	initgraph(b,b,EX_SHOWCONSOLE);
 	IMAGE img;
-	char g[MAX_PATH]; //定义一个字符数组g，大小为MAX_PATH用来获取当前工作目录
-	GetCurrentDirectoryA(MAX_PATH, g); //获取当前工作目录
-	std::cout << "当前工作目录为" << g << std::endl; //输出当前工作目录
-	loadimage(&img, "ATC4.ico"); //加载图片
-	int c = img.getwidth(); //获取图片宽度
-	int d = img.getheight(); //获取图片高度
-	if (c == 0 && d == 0)  /*判断图片是否加载成功*/ { 
-		std::string f = std::string(g) + "/ATC4.ico"; //图片路径
-		loadimage(&img, f.c_str()); //加载图片
-		int c = img.getwidth(); //获取图片宽度
-		int d = img.getheight(); //获取图片高度
-		int e = c/d ;//获取图片宽高比
-	}
-	else {
-		int e = c/d ;//获取图片宽高比
-	}
-	std::cout << "宽高比为" << e << std::endl; //输出图片宽高比
-	Sleep(10000); //延时1秒
-	// initgraph(,);
-	// putimage(b, 0, "/ATC4.ico"); //在屏幕上显示图片
-	// closegraph();
+	loadimage(&img, "ATC4.ico" , b,b,false); //加载图片
+	putimage(0,0 , &img); //在屏幕上显示图片
+	closegraph();
 	return 0;
 }
