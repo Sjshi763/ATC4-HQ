4cdb389326b93264285db3e7cacc2889e36d5c1a
Sjshi763 <qq3258819795@163.com>
2025-06-13 14:36:11 +0800

    fix（正式解决问题)

--- ATC4-HQ.cpp
+++ ATC4-HQ.cpp
@@ -63,7 +63,7 @@ int main() {
 				}
 				// 修改第2行
 				inputFile.close();
-				lines[4 /*这里是前面的数更改的行数*/ /*- 1*/] = banbenhao; // 更新第二行内容";
+				lines[4 /*这里是前面的数更改的行数*/] = banbenhao; // 更新第二行内容";
 				// 写回
 				std::ofstream outputFile("ATC4-HQ.ini", std::ios::trunc);
 				for (const auto& l : lines) outputFile << l << std::endl;
