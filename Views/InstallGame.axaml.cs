1fb0981fbd3e6193fd4eb8b8241cff9669cda46a
Sjshi763 <qq3258819795@163.com>
2025-06-21 22:12:22 +0800

    新功能(安装游戏的UI)

--- Views/InstallGame.axaml.cs
+++ Views/InstallGame.axaml.cs
@@ -4,9 +4,15 @@ namespace ATC4_HQ.Views
 {
     public partial class InstallGame : UserControl
     {
-        public InstallGame ()
+        public InstallGame()
         {
             InitializeComponent();
+        }
+
+
+        private void InstallGame_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+
         }
     }
 }
