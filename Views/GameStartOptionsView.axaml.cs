29717731b381a0910c43964dbecca47568f12b0c
Sjshi763 <qq3258819795@163.com>
2025-07-09 22:10:54 +0800

    写了点注释

--- Views/GameStartOptionsView.axaml.cs
+++ Views/GameStartOptionsView.axaml.cs
@@ -21,7 +21,7 @@ namespace ATC4_HQ.Views
         private async void Button2(object? sender, RoutedEventArgs e)
         {
             Console.WriteLine("安装游戏按钮被点击了，准备打开对话框。");
-
+            //启动窗口
             var dialogWindow = new GameListWindow();
             // 修复 CS8604 警告：确保 owner 是可空类型或显式转换
             Window? ownerWindow = TopLevel.GetTopLevel(this) as Window;
