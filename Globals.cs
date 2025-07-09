84dcd6a797349819918d1a89c8378d684d2e23b0
Sjshi763 <qq3258819795@163.com>
2025-07-09 10:02:17 +0800

    解析ini

---
+++ Globals.cs
@@ -0,0 +1,16 @@
+namespace master
+{
+    namespace Globals
+    {
+        public static class GlobalPaths
+        {
+
+            public static string? MainPath;
+            public static string InitiatorProfileName = @".\ATC4-HQ.ini";
+            public static string Version = "1.0.0";
+            public static string? TransitSoftwareLE;
+            public static string? FirstRun;
+            public static string Keys = "0x5A";
+        }
+    }
+}
